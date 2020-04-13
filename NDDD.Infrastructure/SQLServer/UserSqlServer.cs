144c7ae29ae0e83baa51e41a2221459e60a0ff5b
akira takahashi <akira.takahashi.s2000@gmail.com>
2020-04-13 22:25:04 +0900

    sqlserver

--- NDDD.Infrastructure/SQLServer/UserSqlServer.cs
+++ NDDD.Infrastructure/SQLServer/UserSqlServer.cs
@@ -3,6 +3,7 @@ using NDDD.Domain.Repositories;
 using NDDD.Infrastructure.SqlServer;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,22 +11,30 @@ using System.Threading.Tasks;
 namespace NDDD.Infrastructure.SQLServer
 {
     //internalにしてファクトリークラスからしかNEWできないようにする。
-//    internal sealed class UserSqlServer : IUserRepository
-//    {
-//        public UserEntity GetUserData(int userId)
-//        {
+    internal sealed class UserSqlServer : IUserRepository
+    {
+        public UserEntity GetUserData(int userId)
+        {
 
-//            string sql = @"
-//select UserID,
-//    UserName,
-//    PassWord
-//from UserTable
-//where UserID = @UserID
-//order by UserID
-//LIMIT 1
-//";
-//            return SqlServerHelper.Query();
-
-//        }
-//    }
+            string sql = @"
+select UserID,
+    UserName,
+from UserTable
+where UserID = @UserID
+order by UserID
+LIMIT 1
+";
+            UserEntity user = null;
+            SqlServerHelper.Query(
+                sql,
+                new List<SqlParameter> { new SqlParameter("@UserID", userId) }.ToArray(),
+                                    reader =>
+                                    {
+                                    user = new UserEntity(Convert.ToInt32(reader["UserID"]),
+                                                            Convert.ToString(reader["UserName"]),
+                                                            Convert.ToString(reader["Password"]));
+                                    });
+            return user;
+        }
+    }
 }
